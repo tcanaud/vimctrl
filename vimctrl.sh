42aa0a5db9d02d85a57b054fcf6fac539f0d9fe5
Thibaud CANAUD <tcanaud@e1r5p6.42.fr>
2017-04-17 16:27:46 +0200

    update

--- vimctrl.sh
+++ vimctrl.sh
@@ -1,12 +1,11 @@
 rm -f ~/.vimrc
 cp .vimrc ~/
 rm -rf .vim
-mkdir ~/.vim ~/.vim/colors
+mkdir ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
+git clone https://github.com/vim-scripts/AutoComplPop.git
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/morhetz/gruvbox.git /tmp/gruvbox
 cp /tmp/gruvbox/colors/gruvbox.vim ~/.vim/colors/
-rm -rf /tmp/gruvbox
-echo "CONF VIM FINISH"
